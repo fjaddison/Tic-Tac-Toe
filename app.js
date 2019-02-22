1939442f268f9b6542414cbc79a71f0c42108e7f
Francis Addison <faddison@Franciss-MBP.fios-router.home>
2019-02-22 10:24:36 -0500

    cleaned up a bit and added rest of variables

--- app.js
+++ app.js
@@ -1,33 +1,43 @@
 console.log("testing...");
 
+/* 
+values for HTML elements related to the player turn, all 
+of the game squares with same class, and the grid as a whole
+*/
 const gameSquares = document.querySelectorAll('.game-square');
 const playerTurn = document.querySelector('.player-turn');
+const gameGrid = document.querySelector('.game-grid');
 
+// values for each individual square, row, column, and diagonal
 const sqOne = document.getElementById('s1');
 const sqTwo = document.getElementById('s2');
 const sqThree = document.getElementById('s3');
+const sqFour = document.getElementById('s4');
+const sqFive = document.getElementById('s5');
+const sqSix = document.getElementById('s6');
+const sqSeven = document.getElementById('s7');
+const sqEight = document.getElementById('s8');
+const sqNine = document.getElementById('s9');
 
-const gameGrid = document.querySelector('.game-grid');
+const rowOneArray = [sqOne, sqTwo, sqThree];
+const rowTwoArray = [sqFour, sqFive, sqSix];
+const rowThreeArray = [sqSeven, sqEight, sqNine];
 
-console.log(playerTurn.innerHTML);
-console.log(sqOne);
-console.log(sqOne.style.backgroundColor);
+const colOneArray = [sqOne, sqFour, sqSeven];
+const colTwoArray = [sqTwo, sqFive, sqEight];
+const colThreeArray = [sqThree, sqSix, sqNine];
 
-const rowOne = document.querySelectorAll('.row-one');
-var rowOneArray = [sqOne, sqTwo, sqThree]
-// rowOne.forEach(function(x) {
-//     rowOneArray.push(x)
-// })
+const diagOne = [sqOne, sqFive, sqNine];
+const diagTwo = [sqThree, sqFive, sqSeven];
 
-console.log(rowOneArray)
-
+// event listener for each square to deal with click event
 gameSquares.forEach(function(x) {
     x.addEventListener('click', function() {
-        if (playerTurn.innerHTML === 'X' && x.style.backgroundColor != 'red' && x.style.backgroundColor != 'pink') {
+        if (playerTurn.innerHTML === 'X' && x.style.backgroundColor == '') {
             x.style.backgroundColor = 'pink';
             x.innerHTML = 'X';
             playerTurn.innerHTML = 'O';
-        } if (playerTurn.innerHTML === 'O' && x.style.backgroundColor != 'red' && x.style.backgroundColor != 'pink') {
+        } if (playerTurn.innerHTML === 'O' && x.style.backgroundColor == '') {
             x.style.backgroundColor = 'red';
             x.innerHTML = 'O';
             playerTurn.innerHTML = 'X';
@@ -35,10 +45,19 @@ gameSquares.forEach(function(x) {
     });
 });
 
+/* 
+functions to check if rows, columns, or 
+diagonal arrays are all X or O
+*/
 function isRed(x) {
-    return x.style.backgroundColor == 'red'
+    return x.style.backgroundColor == 'red';
 }
 
+function isPink(x) {
+    return x.style.backgroundColor == 'pink';
+}
+
+// event listener to check if game is over after each turn
 gameGrid.addEventListener('click', function() {
     if (rowOneArray.every(isRed)) {
         console.log('they be red')
